21a39c45b284757be8531835403becd024c02e59
IzakCancino <kazisoyoficial@gmail.com>
2021-06-04 20:57:25 -0500

    Prueba formulario de contacto

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+$para = 'TUCORREO@CORREO.COM';
+$titulo = 'ASUNTO DEL MENSAJE';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+  
+if ($_POST['submit']) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo "<script language='javascript'>
+alert('Mensaje enviado, muchas gracias.');
+window.location.href = 'http://TUSITIOWEB.COM';
+</script>";
+} else {
+echo 'Falló el envio';
+}
+}
+?>
